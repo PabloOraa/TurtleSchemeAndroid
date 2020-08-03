0fccf3dbe93f9502337eccf50a2403f93e4b66cb
Pablo Oraá López <pablito234@live.com>
2020-08-03 15:14:58 +0200

    Change bottom_nav_menu.xml to include the three options of TurtleScheme. Added spanish translation. Added List Icon. Added all necessary references to work with Surface Duo form factor.

--- app/src/main/java/com/example/turtlescheme/MainActivity.java
+++ app/src/main/java/com/example/turtlescheme/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.turtlescheme;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.microsoft.device.dualscreen.bottomnavigation.SurfaceDuoBottomNavigationView;
+import com.microsoft.device.dualscreen.core.DisplayPosition;
+import com.microsoft.device.dualscreen.core.ScreenHelper;
+import com.microsoft.device.dualscreen.core.manager.ScreenModeListener;
+import com.microsoft.device.dualscreen.core.manager.SurfaceDuoScreenManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,10 +17,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    SurfaceDuoScreenManager surfaceDuoScreenManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -22,9 +32,38 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);*/
+        if(ScreenHelper.isDeviceSurfaceDuo(this))
+            configureDualScreen();
     }
 
+    private void configureDualScreen()
+    {
+        surfaceDuoScreenManager = SurfaceDuoScreenManager.getInstance(getApplication());
+        getSurfaceDuoScreenManager().addScreenModeListener(this, new ScreenModeListener()
+        {
+
+            @Override
+            public void onSwitchToSingleScreen()
+            {
+                //((SurfaceDuoBottomNavigationView)findViewById(R.id.nav_view)).setSurfaceDuoDisplayPosition(DisplayPosition.START);
+                Log.d("Surface Duo", "Single Screen");
+            }
+
+            @Override
+            public void onSwitchToDualScreen()
+            {
+                Log.d("Surface Duo", "Dual Screen");
+                //((SurfaceDuoBottomNavigationView)findViewById(R.id.nav_view)).setSurfaceDuoDisplayPosition(DisplayPosition.DUAL);
+            }
+
+        });
+    }
+
+    public SurfaceDuoScreenManager getSurfaceDuoScreenManager()
+    {
+        return surfaceDuoScreenManager;
+    }
 }

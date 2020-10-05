2d73f531c1689b756cc67b0657702202e750e683
Pablo Oraa <pablito234@live.com>
2020-10-05 13:46:30 +0200

    Fix a bug which not allow users from English language to see the web in their language.

--- app/src/main/java/com/example/turtlescheme/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/turtlescheme/ui/notifications/NotificationsFragment.java
@@ -2,6 +2,7 @@ package com.example.turtlescheme.ui.notifications;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.turtlescheme.R;
 
+import java.util.Locale;
 import java.util.Objects;
 
 public class NotificationsFragment extends Fragment
@@ -27,7 +29,11 @@ public class NotificationsFragment extends Fragment
         WebView mywebview = root.findViewById(R.id.webView);
         WebSettings webSettings = mywebview.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mywebview.loadUrl("https://turtlesketch.consulting");
+        Log.d("Language",Locale.getDefault().getDisplayLanguage());
+        if(Locale.getDefault().getLanguage().equals("en"))
+            mywebview.loadUrl("https://turtlesketch.consulting/en-us/index.php");
+        else
+            mywebview.loadUrl("https://turtlesketch.consulting/es-es/index.php");
         mywebview.setWebViewClient(new WebViewClient());
         return root;
     }
